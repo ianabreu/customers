7df56de9392d250120abf569a9605e2c944f0416
Ian Abreu <116256747+ianabreu@users.noreply.github.com>
2024-01-10 09:14:02 -0300

    fix: add host

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const start = async () => {
   await app.register(routes);
 
   try {
-    await app.listen({ port: 3333 });
+    await app.listen({ port: 3333, host: `0.0.0.0` });
   } catch (err) {
     console.log("erro");
 
