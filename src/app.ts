91ebc057e42f654d596fd5176e67b69cda1097a8
Ian Abreu <116256747+ianabreu@users.noreply.github.com>
2024-01-10 10:29:59 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,30 @@
 const port = process.env.PORT || 3000;
 const host = ("RENDER" in process.env) ? "0.0.0.0" : `localhost`;
 
-import fastify from "fastify";
+import fastify from 'fastify';
 
-import cors from "@fastify/cors";
+import cors from '@fastify/cors';
 
-import { routes } from "./route";
+import { routes } from './route';
 
-const app = fastify({ logger: false });
+const app = fastify({ logger : true });
 
 app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message });
-});
+    reply.code(400).send({ message: error.message });
+})
+
+
 
 const start = async () => {
-  await app.register(cors);
-  await app.register(routes);
 
-  await fastify.listen({host: host, port: port }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})
+    await app.register(cors);
+    await app.register(routes);
+
+    try{
+        await app.listen({ port: port, host: host })
+    }catch(err){
+        process.exit(1)
+    }
+}
+
+start();
