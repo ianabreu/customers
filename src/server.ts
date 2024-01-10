596439e3e3a39c3e1eb13937b78c3a3cd475fbb0
ianabreu <ianabreudev@gmail.com>
2024-01-10 12:58:40 -0300

    last update deploy

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { routes } from "./route";
+
+const port = process.env.PORT || 3333;
+const host = "RENDER" in process.env ? "0.0.0.0" : `localhost`;
+
+const app = fastify({ logger: true });
+
+app.setErrorHandler((error, request, reply) => {
+  reply.code(400).send({ message: error.message });
+});
+
+const start = async () => {
+  await app.register(cors);
+  await app.register(routes);
+
+  try {
+    await app.listen({ port: port, host: host });
+  } catch (err) {
+    process.exit(1);
+  }
+};
+
+start();
